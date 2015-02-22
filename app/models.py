8a8669a89f8c7fb1659e22745e7ca5cd90b52779
kozak127 <kozak127@gmail.com>
2015-02-22 14:57:08 +0100

    Added simple task and taskManager models

--- app/models.py
+++ app/models.py
@@ -39,3 +39,45 @@ class Settings(db.Model):
         self.sum_task_delimiter = form.sum_task_delimiter.data
         self.csv_delimiter = form.csv_delimiter.data
         self.csv_divide_weeks = form.csv_divide_weeks.data
+
+
+class Task():
+
+    def __init__(self, project, summary, date, duration):
+        self.project = project
+        self.summary = summary
+        self.date = date
+        self.duration = duration
+
+    def is_in_group(self, group):
+        for project in group:
+            if self.project == project:
+                return True
+        return False
+
+
+class TaskManager():
+
+    def __init__(self):
+        self.tasks = []
+
+    def get_tasks_for_group(self, group):
+        to_return = []
+        for task in self.tasks:
+            if task.is_in_group(group):
+                to_return.append(task)
+        return to_return
+
+    def get_tasks_for_group(self, group, date):
+        to_return = []
+        for task in self.tasks:
+            if task.date == date and task.is_in_group(group):
+                to_return.append(task)
+        return to_return
+
+    def get_tasks_for_group(self, group, date_start, date_end):
+        to_return = []
+        for task in self.tasks:
+            if date_start <= task.date <= date_end and task.is_in_group(group):
+                to_return.append(task)
+        return to_return
