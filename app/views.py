c221dffe08c82809ef3ba5c6528be50e1ee84962
kozak127 <kozak127@gmail.com>
2015-02-22 13:38:30 +0100

    Removed some scraps of babel internationalization

--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, \
     login_required
-from flask.ext.babel import gettext
 from app import app, db, lm, oid
 from app.forms import LoginForm, SettingsForm
 from app.models import User, Settings
